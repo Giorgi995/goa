ec0a81691d5384047ce212c695ab3ea600c0649c
Giorgi995 <giozakaraia057@gmail.com>
2025-06-24 23:39:34 +0400

    first commit

---
+++ level 06/homework/.py
@@ -0,0 +1,36 @@
+case-sensitive--> ამ დროს ექცევა ყურადყება პატარა და დიდი ასოებს
+snake case--> dog_name="Jeki" 
+
+საუკეთესო პრაკტიკები გვჯირდება იმისთვის რომ  ადვილად იყოს გასაგები სხვა ადამიანებისთვის თქვენი დაწერილი კოდი
+
+num1 = input(Enter the first number:)
+num2 = input(Enter the second number:)
+print(num1+num2)
+print(num1+num2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

36ee891590eac6bd01abf28c3daeedc247c50f32
Giorgi995 <giozakaraia057@gmail.com>
2025-10-22 16:55:23 +0400

    commit

---
+++ level 71/homework/hw6.js
@@ -0,0 +1,12 @@
+function manualRaverse(arr){
+    return arr.manualRaverse
+}
+console.log(manualRaverse([1,2,3,4,5]))
+
+function manualslice(arr, a,b){
+    let result=[]
+    for (let i=a; i<b; i++){
+        result.push(arr[i])
+    }
+    return result
+}

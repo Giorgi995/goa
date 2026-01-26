050414afa35b99d6f2e10c4ce67ab640f4f569d6
Giorgi995 <giozakaraia057@gmail.com>
2026-01-26 18:27:28 +0400

    commit

---
+++ level 98/classwork/cw1.jsx
@@ -0,0 +1,11 @@
+ function App(){
+  return (
+    <>
+      <div className="h-screen flex-center justify-center">
+        <h1>"text-[50px] text-[blue]"Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ level 98/classwork/cw2.jsx
@@ -0,0 +1,82 @@
+function App() {
+  const objectStyle = {
+    backgroundColor: "green",
+    color: "green",
+    padding: "15px",
+    marginTop: "20px"
+  };
+
+  return (
+    <div>
+      {/* Inline styling */}
+      <h1
+        style={{
+          backgroundColor: "green",
+          color: "green",
+          padding: "15px"
+        }}
+      >
+        Inline Styling
+      </h1>
+
+      {/* Object variable styling */}
+      <h1 style={objectStyle}>
+        Object Variable Styling
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+Collapse
+Collapse (40 lines)
+View whole file
+cw.jsx
+cw.jsx (2 KB)
+2 KB
+cw.jsx (2 KB)
+Download cw.jsx (1 KB)Change language
+
+2
+Add Reaction
+:joy:
+Click to react
+:thumbsup:
+Click to react
+:heart:
+Click to react
+Add Reaction
+Reply
+Forward
+More
+
+ Luka Shavadze(Shavidze) — 10:53 PMSunday, January 25, 2026 10:53 PM
+
+
+1
+Add Reaction
+
+ტარიელ ბეჟანიშვილი(Shavidze-ს) — 10:53 PMSunday, January 25, 2026 10:53 PM
+
+
+1
+Add Reaction
+
+ლუკა ლეფსვერიძე (ცხადაძე)ჯგუფის საუკეთესო მოსწავლე N3 (bronze) — 10:51 PMSunday, January 25, 2026 10:51 PM
+
+
+1
+Add Reaction
+:joy:
+Click to react
+:thumbsup:
+Click to react
+:heart:
+Click to react
+Add Reaction
+Reply
+Forward
+More
+
+ლაზარე თიგიშვილი(ქორიძეს) — 10:52 PMSunday, January 25, 2026 10:52 PM
+
